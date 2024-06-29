2c4efb7942ac681c4bc2b278229c2e4752b28261
Samuel Eduardo <eduardosamuel173@gmail.com>
2024-06-29 14:06:30 -0300

    Hello Word

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -1,21 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package gitexemplo;
 
-/**
- *
- * @author Samuel
- */
 public class GitExemplo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       Project Location
-               
+        System.out.println("Hello Word !!!");
     }
     
 }
